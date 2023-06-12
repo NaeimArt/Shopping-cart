81c58a2f9d4888254a7290685ccef3af93e9a07d
Naeim <119408921+NaeimArt@users.noreply.github.com>
2023-06-12 10:45:00 +0330

    first commit

---
+++ Add-to-Cart/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelectorAll(".btn");
+      btn.forEach(button => {
+        button.addEventListener('click', btnFunction)
+      })
+
+      function btnFunction(){
+        let button = this ;
+        button.classList.add('clicked');
+      }
